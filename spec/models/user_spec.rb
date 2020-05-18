394fe9b3e02e634869b0716a18dd8888c814435f
Bruno02s <brunolourenco02s@gmail.com>
2020-05-18 18:30:00 -0300

    Adding show action to ther users controller

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   let(:user) { build(:user) }
   
-  #Testa as validações do model principal
+  #Tests the validations of the main model
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
   it { is_expected.to allow_value('brunolourenco02s@gmail.com').for(:email) }
